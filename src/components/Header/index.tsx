503c4baf1f1a3a4da9cd6cd46ef3491734ac910b
Everton <everton.aaraujo@outlook.com>
2021-05-26 19:57:57 -0300

    Modal created using react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,19 @@ import {
   Content
 } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type='button'>Nova transação</button>
+        <button type='button' onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
-  )
+  );
 }
